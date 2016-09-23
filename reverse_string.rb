dc8948ba50f28d97d1a2cd95b61c01d07ebaf81c
Linh Nguyen My <lnguyenmy1@gmail.com>
2016-09-23 20:10:05 +0100

    Complete code

--- reverse_string.rb
+++ reverse_string.rb
@@ -1,5 +1,11 @@
 def reverse_string(string)
-  split_str = string.split(" ")
+  split_string = string.split("")
+  reversed = []
+  string.size.times { reversed << split_string.pop }
+  reversed.join
 end
 
 p reverse_string("Hello, my name is PINGLINH.")
+
+# last_letter = split_str[split_str.size-1]
+# second_last_letter = split_str[split_str.size-2]
